1201095a8f6efef6ed947e41c8f45f3debfa5d70
Ivan Psarew <psarew.ivan@yandex.ru>
2024-09-13 20:06:57 +0600

    fix: cards styles

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -3,7 +3,7 @@ import styles from './Card.module.scss';
 
 export function Card({ title, content, index }) {
     return (
-        <div className={styles['card']}>
+        <div className={`${styles['card']} ${styles[`card_${index}`]}`}>
             <div className={styles['card__title']}>{title}</div>
             <div className={styles['card__content']}>{content}</div>
             <div className={styles['card__index']}>{index}</div>
