893e363938d4d1e9efaf56039f929d276fc70ce5
Ivan Psarew <psarew.ivan@yandex.ru>
2024-09-19 19:07:19 +0600

    feat: add url query for popup opening

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Promo from '../components/Promo';
 import Cards from '../components/Cards';
 import Crm from '../components/Crm';
@@ -16,8 +17,17 @@ export async function getStaticProps() {
 }
 
 export default function Index() {
+    const router = useRouter();
     const [hasConnect, setHasConnect] = useState(false);
     useScrollFreeze(hasConnect);
+
+    useEffect(() => {
+        const { reg } = router.query;
+
+        if (reg === 'true') {
+            setHasConnect(true);
+        }
+    }, [router.query]);
 
     const handleConnectClick = useCallback(() => {
         setHasConnect((prev) => !prev);
